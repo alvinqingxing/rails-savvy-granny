a5eb900afa68691c080fb466c25af1cac89e83a3
Alvin Lim <alvinch@hawaii.edu>
2020-07-07 16:35:00 +0800

    Created Reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'reviews/show'
+  get 'reviews/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
