a0be9c0a41bbabd885fb07825173159c4e249725
Alvin Lim <alvinch@hawaii.edu>
2020-07-07 16:00:31 +0800

    Replaced Sender column in Messages with User foreign key

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,3 @@
 class Message < ApplicationRecord
-  belongs_to :sender, class_name :user, foreign_key :sender
   belongs_to :chatroom
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_073143) do
+ActiveRecord::Schema.define(version: 2020_07_07_075731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,10 @@ ActiveRecord::Schema.define(version: 2020_07_07_073143) do
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.boolean "read"
-    t.integer "sender", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +56,5 @@ ActiveRecord::Schema.define(version: 2020_07_07_073143) do
   end
 
   add_foreign_key "jobs", "categories"
+  add_foreign_key "messages", "users"
 end
