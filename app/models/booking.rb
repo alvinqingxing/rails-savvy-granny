ac058c058d3ce2d3744dbca977d4dc4555613cc8
Alvin Lim <alvinch@hawaii.edu>
2020-07-07 16:25:16 +0800

    Generated Review Model & Schema

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :job
   belongs_to :user
   has_many :chatrooms
+  has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_081631) do
+ActiveRecord::Schema.define(version: 2020_07_07_082159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2020_07_07_081631) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +95,5 @@ ActiveRecord::Schema.define(version: 2020_07_07_081631) do
   add_foreign_key "jobs", "categories"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "reviews", "bookings"
 end
