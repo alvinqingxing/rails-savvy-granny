a08ec12815c07a95c95e1d23519619dfc02d9d99
Alvin Lim <alvinch@hawaii.edu>
2020-07-07 16:29:35 +0800

    Renamed user_id column in Messages to Sender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_082159) do
+ActiveRecord::Schema.define(version: 2020_07_07_082611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,10 +58,10 @@ ActiveRecord::Schema.define(version: 2020_07_07_082159) do
     t.boolean "read"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
+    t.bigint "sender", null: false
     t.bigint "chatroom_id", null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
+    t.index ["sender"], name: "index_messages_on_sender"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -94,6 +94,6 @@ ActiveRecord::Schema.define(version: 2020_07_07_082159) do
   add_foreign_key "chatrooms", "bookings"
   add_foreign_key "jobs", "categories"
   add_foreign_key "messages", "chatrooms"
-  add_foreign_key "messages", "users"
+  add_foreign_key "messages", "users", column: "sender"
   add_foreign_key "reviews", "bookings"
 end
