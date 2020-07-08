17f35046acbf9efc9bb66382f23eb031c9486812
Alvin Lim <alvinch@hawaii.edu>
2020-07-08 15:36:51 +0800

    Set up Pundit including Booking policy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,25 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
   def index
+    @bookings = policy_scope(Booking)
   end
 
   def new
+    @booking = current_user.bookings.new
+    authorize @booking
   end
 
   def create
+    @booking = current_user.bookings.new(booking_params)
+    authorize @booking
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,8 +29,28 @@ class BookingsController < ApplicationController
   end
 
   def update
+    @booking.update(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :price, :status, :transaction_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+    authorize @booking
   end
 end
