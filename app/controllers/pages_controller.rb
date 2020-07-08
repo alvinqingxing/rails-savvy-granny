86fe2fc4ba2ea5851cabee95074fc4d048e95cb4
Evan Ang <alethiologist@gmail.com>
2020-07-08 17:44:56 +0800

    moved around stuff on navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    authorize @page
     @jobs = Job.all
     @new_user = User.new
   end
