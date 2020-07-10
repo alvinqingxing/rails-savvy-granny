62d0ad15d9acdc0f8a94d3052f101755f3ac479b
yjung <yjung519@gmail.com>
2020-07-10 14:48:52 +0900

    search bar & jobs index page

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+#add saerch gem
+gem 'pg_search', '~> 2.3.0'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ApplicationRecord
   has_many :jobs
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :title, :syllabus ],
+    associated_against: {
+      director: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
