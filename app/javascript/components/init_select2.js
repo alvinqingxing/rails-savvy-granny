5fffa7988e289840c6d2912b1c7a96ad79c34e93
ningfei ou <ouningfei@gmail.com>
2020-07-13 12:57:36 +0800

    copy to clipboard shortcut

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -7,4 +7,20 @@ const initSelect2 = () => {
   });
 };
 
+function videoCall() {
+  /* Get the text field */
+  var copyText = document.getElementById("videocall");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied to clipboard: " + copyText.value);
+}
+
 export { initSelect2 };
+export { videoCall };
