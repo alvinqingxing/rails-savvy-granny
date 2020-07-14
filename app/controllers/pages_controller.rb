b2787bc0fcc493b2b406f980a04c06a0ba9ddc9b
yjung <yjung519@gmail.com>
2020-07-14 12:33:04 +0900

    notification add & chat error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,4 +19,18 @@ class PagesController < ApplicationController
 
     # @chatrooom = Chatrooom.create(booking: ##) 
   end
+
+  def notification
+    @user = current_user
+    if @user.tutor?
+      # shuld be availble to see booking name & booking status is updated
+      # chatroom is updated 
+       
+      #if booking status is updated, we should display, should I do that in model? as we discussed? 
+    
+    else 
+
+    
+    end 
+  end
 end
