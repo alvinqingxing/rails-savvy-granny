13637f911dab031cf497198d75e9ee56e23f885d
yjung <yjung519@gmail.com>
2020-07-14 17:34:21 +0900

    notification for msg

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [ :show ]
 
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [ :show ]
+
   
   root to: 'pages#home'
   resources :jobs, only: [ :index, :show ]
