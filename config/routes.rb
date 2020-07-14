0455f4c6979e12fbd9fab0e0f2db251b041f6499
Alvin Lim <alvinch@hawaii.edu>
2020-07-14 20:43:44 +0800

    Stripe & Stripe webhook are working

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,20 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
+
   resources :users, only: [ :show ]
   resources :jobs, only: [ :index, :show ]
   resources :categories, only: [ :index ]
+  resources :reviews, only: [ :new, :create, :show, :destroy ]
+
+  resources :chatrooms, only: [] do
+    resources :messages, only: [:create, :destroy]
+  end
+
   resources :orders, only: [ :show, :create ] do
     resources :payments, only: :new
   end
+
   resources :bookings do
     member do
       get :apply
@@ -16,15 +24,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :chatrooms, only: [] do
-    resources :messages, only: [:create, :destroy]
-  end
-
   get "/bookings/:booking_id/chatroom", to: "chatrooms#show", as: :booking_chatroom
-
-  resources :reviews, only: [ :new, :create, :show, :destroy ]
-  mount StripeEvent::Engine, at: '/stripe-webhooks'
   get "dashboard", to: "pages#dashboard", as: :dashboard
   get '/user' => "pages#dashboard", :as => :user_root
   get "/bookings/new" => "orders#new", :as => :bookings_new
+
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
 end
